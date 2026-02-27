7d256125d9a4b1bb73ce6dd4937801f568b3328f
chuscomr <chuscomr@gmail.com>
2026-02-27 15:33:58 +0100

    Update __init__.py

--- indicadores/__init__.py
+++ indicadores/__init__.py
@@ -2,10 +2,18 @@
 from flask import Blueprint
 
 indicadores_bp = Blueprint(
-    'indicadores',
+    "indicadores",
     __name__,
-    template_folder='templates',
-    static_folder='static'
+    url_prefix="/indicadores",
+    template_folder="templates",
+    static_folder="static",
 )
+
+@indicadores_bp.after_request
+def no_cache_indicadores(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 
 from . import routes, api
